d2e526f8faa772da26d5283e2676bce22873c95a
dking <dking@db907d3f-4b15-4875-922b-0a53dc4e8d83>
2012-09-21 00:30:10 +0000

    Set executable bit on scripts. Pull out top-level namespace from lcg_transmission_params so it can be changed. All joint name to be specified in set_pos script.

--- gripper_control/scripts/lcg_set_pos.py
+++ gripper_control/scripts/lcg_set_pos.py
@@ -7,26 +7,30 @@ import pr2_controllers_msgs.msg
 
 import os
 
-def setLCGPosition(desired_position, force):
+
+def setLCGPosition(desired_position, force, name_prefix):
 	print "LCGPosition"
-	pub = rospy.Publisher('/l_gripper_controller/command', pr2_controllers_msgs.msg.Pr2GripperCommand, latch=True)
+	pub = rospy.Publisher('/%s_gripper_controller/command'%name_prefix, pr2_controllers_msgs.msg.Pr2GripperCommand, latch=True)
 	rospy.init_node('lcg_setpoint')
 	rospy.sleep(1.0)	
 	pub.publish(position = desired_position, max_effort=force)
 	print "LCG set position: %.04f, force %.03f" % (desired_position, force)
+	rospy.sleep(1.0)	
+
 
 if __name__ == '__main__':
 
-	os.environ['ROS_MASTER_URI'] = 'http://prq1:11311'
+	#os.environ['ROS_MASTER_URI'] = 'http://prq1:11311'
 
 	parser = argparse.ArgumentParser(description='Set point and max force for a PR2 low cost gripper.')
 	parser.add_argument('-f', type=float, help='Desired gripping force.', required=True)
 	parser.add_argument('-p', type=float, help='Desired position setpoint (0.0 to 0.135 on the LCG).', required=True)
+	parser.add_argument('-n', type=str, help='Change name prefix for gripper', default='l')
 
 	args = parser.parse_args()
 
 	try:
-		setLCGPosition(args.p,args.f)
+		setLCGPosition(args.p,args.f,args.n)
 	except rospy.ROSInterruptException: 
 		print "ROSInterruptException"
 		pass
